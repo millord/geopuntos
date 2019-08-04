9f2e4e69495ce6913fac4ffb0e8ac6d17ec5e22e
millord <juancarlosmillord@gmail.com>
2019-08-04 08:15:17 -0400

    better green for login

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -35,7 +35,7 @@ const Login = ({ classes }) => {
   };
 
   return (
-    <div style={{ backgroundColor: "#46f257" }} className={classes.root}>
+    <div style={{ backgroundColor: "#14a10d" }} className={classes.root}>
       <Typography
         component="h1"
         variant="h3"
